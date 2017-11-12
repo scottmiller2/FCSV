b32312959b65c571bffb07d4a69713921711896d
scottmiller2 <scott.miller2@stvincent.edu>
2017-11-12 13:42:46 -0500

    Copy WIP to Master

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,3 +1,8 @@
 export const DB_CONFIG = {
-
+    apiKey: "AIzaSyBdELXqTFPmcEEmz7PBapoVsZm7EbKmF6s",
+    authDomain: "fantsy-ae03e.firebaseapp.com",
+    databaseURL: "https://fantsy-ae03e.firebaseio.com",
+    projectId: "fantsy-ae03e",
+    storageBucket: "fantsy-ae03e.appspot.com",
+    messagingSenderId: "680143355664"
   };
