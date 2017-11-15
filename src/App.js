bd5fb6d33f2f188dcac738b593d80c389c155a78
scottmiller2 <scott.miller2@stvincent.edu>
2017-11-15 15:31:03 -0500

    Changes to make the page more mobile friendly

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const auth = firebase.auth();
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.addPlayer = this.addPlayer.bind(this);
@@ -33,7 +33,7 @@ class App extends Component {
     this.players = [];
   }
 
-  componentWillMount(){
+  componentWillMount() {
     const previousPlayers = this.state.players;
 
     this.database.on('child_added', snap => {
@@ -43,106 +43,106 @@ class App extends Component {
         votes: snap.val().votes,
         rank: snap.val().rank,
       })
-   
-    
-    this.database.on('child_changed', function(snapshot) {
-      var name = snapshot.val();
-      console.log("Player: " + name.playerContent + " has  " + name.votes + " votes.")
-    })
 
-    this.setState({
-      players: previousPlayers
-      }); 
-  })
-}
+
+      this.database.on('child_changed', function (snapshot) {
+        var name = snapshot.val();
+        console.log("Player: " + name.playerContent + " has  " + name.votes + " votes.")
+      })
+
+      this.setState({
+        players: previousPlayers
+      });
+    })
+  }
   componentDidMount() {
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      this.setState({ user });
-      } 
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
     });
   }
-  addPlayer(player){
-    
-      this.state.user ?
-      this.database.push().set({ playerContent: player, votes: 0, rank: 0})
-    :
+  addPlayer(player) {
+
+    this.state.user ?
+      this.database.push().set({ playerContent: player, votes: 0, rank: 0 })
+      :
       console.log("Not Logged In")
-    
+
   }
 
-  byeTab(){
-    this.setState({byeTabVisible: !this.state.byeTabVisible})
+  byeTab() {
+    this.setState({ byeTabVisible: !this.state.byeTabVisible })
   }
 
   closeByeTab() {
     var x = document.getElementById("byes");
     if (x.style.display === "none") {
-        x.style.display = "block";
+      x.style.display = "block";
     } else {
-        x.style.display = "none";
+      x.style.display = "none";
     }
   }
 
   closeWeekTab() {
     var x = this.refs.weekHeading;
     if (x.display === "none") {
-        x.display = "block";
+      x.display = "block";
     } else {
-        x.style.display = "none";
+      x.style.display = "none";
     }
-}
+  }
 
   //Trending influence
-  downvotePlayer(playerId){
-    
+  downvotePlayer(playerId) {
+
     this.state.user ?
-    this.database.child(playerId).transaction(function (player) {
+      this.database.child(playerId).transaction(function (player) {
         if (player) {
-            player.votes--
+          player.votes--
         }
         return player;
-    })
-    :
-    console.log("Must log in to vote")
-    
+      })
+      :
+      console.log("Must log in to vote")
+
   }
 
   userLogOut() {
     auth.signOut()
-    .then(() => {
-      this.setState({
-        user: null
+      .then(() => {
+        this.setState({
+          user: null
+        });
       });
-    });
   }
 
   userLogIn() {
-    auth.signInWithPopup(provider) 
+    auth.signInWithPopup(provider)
       .then((result) => {
-       const user = result.user;
-       this.setState({
-        user
+        const user = result.user;
+        this.setState({
+          user
+        });
       });
-    });
   }
 
-  upvotePlayer(playerId){
+  upvotePlayer(playerId) {
     this.state.user ?
       this.database.child(playerId).transaction(function (player) {
-       if (player) {
-            console.log(player)
-            player.votes++
+        if (player) {
+          console.log(player)
+          player.votes++
         }
-       return player;
-    })
-    :
-    console.log("Must be logged in to vote.")
-    
+        return player;
+      })
+      :
+      console.log("Must be logged in to vote.")
+
   }
 
-  weekTab(){
-    this.setState({weekTabVisible: !this.state.weekTabVisible})
+  weekTab() {
+    this.setState({ weekTabVisible: !this.state.weekTabVisible })
   }
 
   render() {
@@ -153,74 +153,76 @@ class App extends Component {
     let hideWeek = this.state.weekTabVisible ? "none" : "block"
     return (
       <div className="playersWrapper">
+        <div className="userBar">
+        {
+          this.state.user ?
+            <div className='user-profile'>
+              <img className='profile-image' onClick={this.userLogOut} src={this.state.user.photoURL} alt={"userphoto"} />
+            </div>
+            :
+            console.log("You must be logged in to contribute.")
+        }
+        {
+          this.state.user ?
+            console.log("")
+            :
+            <div className="authArea"><button className="loginSignUpOut" onClick={this.userLogIn}>Sign In</button></div>
+        }
+        </div>
         <div className="playersHeader">
-        {
-        this.state.user ?
-        <div className='user-profile'>
-        <img className='user-profile' onClick={ this.userLogOut } src={this.state.user.photoURL} alt={"userphoto"}/>
-      </div>
-        :
-        console.log("You must be logged in to contribute.")
-        }
-        {
-        this.state.user ?
-        console.log("")
-        :
-        <span className="authArea"><button className="loginSignUpOut" onClick={this.userLogIn}>Sign In</button></span>
-        }
-          <div className="heading">Fantsy <img src={require('./Static/img/4.png'  ) } 
-          style={{width: 65, height: 43}} alt={"background"}/>
-          <div className="subheading">Crowdsourced Player trends</div>
+          <div className="heading">Fantsy <img src={require('./Static/img/4.png')}
+             className="fantsy-image" alt={"background"} />
+            <div className="subheading">Crowdsourced Player trends</div>
           </div>
-          
-          </div>
-          
+
+        </div>
+
 
         <div className="playersFooter">
-          <PlayerForm addPlayer={this.addPlayer}/>
+          <PlayerForm addPlayer={this.addPlayer} />
         </div>
-       
-       <span style={{display: hideWeek}} className="weekHeading">Week 11 — Thursday Night Football — Seahawks vs. Cardinals <a style={{display: hideWeek}} className="closeTab" onClick={this.weekTab.bind(this)}>x</a></span>
-        <span style={{display: hideBye}} className="byes">Byes — Carolina, Indianapolis, New York Jets, San Francisco <a style={{display: hideBye}} className="closeTab" onClick={this.byeTab.bind(this)}>x</a></span>
+
+        <span style={{ display: hideWeek }} className="weekHeading">Week 11 — Thursday Night Football — Seahawks vs. Cardinals <a style={{ display: hideWeek }} className="closeTab" onClick={this.weekTab.bind(this)}>x</a></span>
+        <span style={{ display: hideBye }} className="byes">Byes — Carolina, Indianapolis, New York Jets, San Francisco <a style={{ display: hideBye }} className="closeTab" onClick={this.byeTab.bind(this)}>x</a></span>
         <div className="playersColumns">
-        <div className="playersBody">
-          <span className="trendHeaderUp">TRENDING UP</span>
-          {
-            orderedPlayersUp.map((player) => {
-              return (
-            <Player 
-            playerContent={player.playerContent}
-            playerId={player.id}
-            key={player.id}
-            upvotePlayer={this.upvotePlayer}
-            downvotePlayer={this.downvotePlayer}
-            userLogIn = {this.userLogIn}
-            userLogOut = {this.userLogOut}
-            />
-              )
-            })
-          }
+          <div className="playersBody">
+            <span className="trendHeaderUp">TRENDING UP</span>
+            {
+              orderedPlayersUp.map((player) => {
+                return (
+                  <Player
+                    playerContent={player.playerContent}
+                    playerId={player.id}
+                    key={player.id}
+                    upvotePlayer={this.upvotePlayer}
+                    downvotePlayer={this.downvotePlayer}
+                    userLogIn={this.userLogIn}
+                    userLogOut={this.userLogOut}
+                  />
+                )
+              })
+            }
+          </div>
+          <div className="playersBody">
+            <span className="trendHeaderDown">TRENDING DOWN</span>
+            {
+              orderedPlayersDown.map((player) => {
+                return (
+                  <Player
+                    playerContent={player.playerContent}
+                    playerId={player.id}
+                    key={player.id}
+                    upvotePlayer={this.upvotePlayer}
+                    downvotePlayer={this.downvotePlayer}
+                    userLogIn={this.userLogIn}
+                    userLogOut={this.userLogOut}
+                  />
+                )
+              })
+            }
+          </div>
         </div>
-        <div className="playersBody">
-        <span className="trendHeaderDown">TRENDING DOWN</span>
-          { 
-            orderedPlayersDown.map((player) => {
-              return (
-            <Player 
-            playerContent={player.playerContent}
-            playerId={player.id}
-            key={player.id}
-            upvotePlayer={this.upvotePlayer}
-            downvotePlayer={this.downvotePlayer}
-            userLogIn = {this.userLogIn}
-            userLogOut = {this.userLogOut}
-            />
-              )
-            })
-          }
-          </div>
-          </div>
-        </div> //playersWrapper
+      </div> //playersWrapper
     );
   }
 }
