d47592eb3c08d8f0cff7944fe26bf951b6bf1166
scottmiller2 <scott.miller2@stvincent.edu>
2018-05-21 03:52:42 -0400

    New navigation bar, responsive changes

--- functions/index.js
+++ functions/index.js
@@ -1,53 +1,92 @@
 const functions = require('firebase-functions');
+var admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-var rank = functions.database.ref('players/{playerId}/votes')
-.onUpdate((change, context) => {
-  console.log("Business")
-  var orderedListRef = change.after.ref.root.child('players').orderByChild('votes')
-  var oldVotes = change.before.val()
-  var newVotes = change.after.val()
-  var changeRank = 0
+exports.votePlayer = functions.https.onCall((request, response) => {
+    const playerToVote = request.query.playerToVote;
 
-  // went higher in the list so bump every player passed by 1
-  if (newVotes > oldVotes) {
-    orderedListRef = orderedListRef.endAt(newVotes)
-    changeRank = 1
-  } else {// went lower in the list so bump every player passed by -1
-    orderedListRef = orderedListRef.startAt(oldVotes)
-    changeRank = -1
-  }
-  return orderedListRef.once('value')
-  .then((ss) => {
-    var promises = []
-    var newRank = -1
-    var currentRank = -1
-    // IMPORTANT: must use `forEach` to ensure proper order
-    ss.forEach((playerSS) => {
-      if (playerSS.key === context.params.playerId) {
-        currentRank = playerSS.child('rank').val()
-        return
-      }
-      if (playerSS.child('votes').val() === newVotes) {
-        newRank = playerSS.child('rank').val()
-        return
-      }
-      // only bump players passed in the change
-      if (changeRank === 1 && playerSS.child('votes').val() < oldVotes) {
-        return
-      } else if (changeRank === -1 && playerSS.child('votes').val() > oldVotes) {
-        return
-      }
-      // use transaction to ensure proper number of bumps if multiple changes at once
-      promises.push(playerSS.child('rank').ref.transaction((rank) => {
-        return rank + changeRank
-      }))
-    })
-    var diff = newRank - currentRank
-    // use transaction paired with diff just in case this is also being bumped
-    promises.push(change.before.ref.parent.child('rank')
-    .transaction((rank) => {
-      return rank + diff
-    }))
-    return Promise.all(promises)
-  })
+    if (playerToVote === null) {
+        return response.status(404).send({
+            "success": false,
+            "message": "Missing parameter: `playerToVote: String`."
+          });
+    }
+
+    const userWhoVote = request.query.userWhoVote;
+    if (userWhoVote === null) {
+        return response.status(404).send({
+            "success": false,
+            "message": "Missing parameter: `userWhoVote: String`."
+          });
+    }
+
+    const upVote = request.query.upVote;
+    if (upVote === null) {
+        return response.status(404).send({
+            "success": false,
+            "message": "Missing parameter: `upVote: Boolean`."
+        });
+    }
+
+    const rootReference = admin.database().ref();
+    return rootReference.child('playersVotes/' + playerToVote + '/' + userWhoVote).once('value', function(snapshot) {
+        if (snapshot.val()) {
+            return response.status(404).send({
+                "success": false,
+                "message": "You already voted for this player."
+            });
+        } else {
+            var promises = [];
+            const saveUserIDPromise = rootReference.child('playersVotes/' + playerToVote + '/' + userWhoVote).set(true)
+            promises.push(saveUserIDPromise)
+            const updateVotesPromise = rootReference.child('players/' + playerToVote + '/votes').transaction(function(currentValue) {
+                if (currentValue === null) {
+                    return 0;
+                }
+                if (upVote === true) {
+                    return currentValue + 1;
+                } else {
+                    return currentValue - 1;
+                }
+            });
+            promises.push(updateVotesPromise)
+            
+            Promise.all(promises).then(() => {
+                return response.status(200).send({
+                    "success": true
+                })
+            }).catch(error => {
+                console.error(error);
+            });                
+        }
+    });
+});
+
+exports.getTopPlayers = functions.https.onRequest((request, response) => {
+    var playersArray = [];
+    const rootReference = admin.database().ref();
+
+    return rootReference.child('players').once('value', function(snapshot) {
+        snapshot.forEach(function(aSnapshot) {
+            playersArray.push(aSnapshot.val())
+        })
+
+        playersArray = playersArray.sort(function(first, second) {
+            return first.votes < second.votes;
+        });
+
+        var topVotes = playersArray[0]["votes"];
+        var rankNumber = 1;
+        
+        playersArray[0]["rank"] = rankNumber;
+        playersArray.forEach(function (aPlayer) {
+            if (aPlayer.votes < topVotes) {
+                rankNumber += 1;
+                topVotes = aPlayer.votes;
+            }
+            aPlayer["rank"] = rankNumber;
+        });
+
+        return response.status(200).send(playersArray);
+    });
 })
